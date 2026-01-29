79d9bd759fe5cccd9dca0a8bf968e9744acf2bcd
Sandu Bogdan <cornusandu@hotmail.com>
2026-01-29 11:38:38 +0200

    additions

--- src/sysq-lib/lib.cpp
+++ src/sysq-lib/lib.cpp
@@ -3,14 +3,48 @@
 #include <string>
 #include <filesystem>
 #include <system_error>
+#include <dlfcn.h>
+#include <cstdlib>
+#include <cstring>
+
+enum class ServiceType : size_t {
+    Type_BIN = 1,
+    Type_SO  = 2,
+};
 
 struct ServiceMetadata {
     unsigned char boot_priority;
     const char* path;
+    ServiceType type;
 };
 
-extern "C" ServiceMetadata* parse_so(const char* path) {
-    return 0x0;
+extern "C" ServiceMetadata* parse_so(void* handle) {
+    void* tmp = NULL;
+    ServiceMetadata *data = (ServiceMetadata*)malloc(sizeof(ServiceMetadata));
+    memset(data, 0, sizeof(ServiceMetadata));
+
+    tmp = dlsym(handle, "boot_priority");
+    if (tmp) data->boot_priority = *(char*)tmp;
+    else     data->boot_priority = 1;
+
+    tmp = (void*)*(char**)dlsym(handle, "path");
+    if (tmp && data->path == NULL) data->path = (const char*)tmp;
+    tmp = (void*)*(char**)dlsym(handle, "bpath");
+    if (tmp && data->path == NULL) data->path = (const char*)tmp;
+    tmp = (void*)*(char**)dlsym(handle, "loc");
+    if (tmp && data->path == NULL) data->path = (const char*)tmp;
+
+    if (data->path == NULL) return 0x0;
+
+    tmp = dlsym(handle, "type");
+    if (!tmp) data->type = ServiceType::Type_BIN;
+    else {
+        if (*(ServiceType*)tmp == ServiceType::Type_BIN)     data->type = ServiceType::Type_BIN;
+        else if (*(ServiceType*)tmp == ServiceType::Type_SO) data->type = ServiceType::Type_SO;
+        else                                                 data->type = ServiceType::Type_BIN;     
+    }
+
+    return data;
 }
 
 namespace fs = std::filesystem;
--- src/systemq/main.cpp
+++ src/systemq/main.cpp
@@ -9,33 +9,39 @@
 
 namespace fs = std::filesystem;
 
+enum class ServiceType : size_t {
+    Type_BIN = 1,
+    Type_SO  = 2,
+};
+
 struct ServiceMetadata {
     unsigned char boot_priority;
     const char* path;
+    ServiceType type;
 };
 
-using parse_so_fn = ServiceMetadata*(*)(const char*);
+using parse_so_fn = ServiceMetadata*(*)(void*);
 using get_files_fn = std::vector<std::string>(*)(const char*);
 
 parse_so_fn parse_so = NULL;
 get_files_fn get_files = NULL;
 
-void reap_zombies() {
+pid_t reap_zombies() {
     int status;
     pid_t pid;
 
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-
+        return pid;
     }
+    return 0;
 }
 
-void reap_zombies_blocking() {
+pid_t reap_zombies_blocking() {
     int status;
     pid_t pid;
 
-    while ((pid = waitpid(-1, &status, 0)) > 0) {
-
-    }
+    pid = waitpid(-1, &status, 0);
+    return pid;
 }
 
 int main() {
@@ -53,4 +59,37 @@ int main() {
     get_files = (get_files_fn)dlsym(sysq_lib, "get_files");
     printf("sysq: obtained sysq_lib->get_files at %p\n", get_files);
     if (get_files == NULL) exit(1);
+
+    std::vector<std::string> services = get_files("/pboot/systemq-data");
+
+    if (services.empty()) {
+        printf("sysq: no services found\nsysq: starting bash\n");
+        if (fork()) {
+            while (reap_zombies_blocking()) {;;}
+        }
+        else {
+            execv("/bin/bash", (char*[]){"/bin/bash", NULL});
+            printf("sysq-bash: execv failed\n");
+            exit(1);
+        }
+    }
+
+    ServiceMetadata* metadata = (ServiceMetadata*)calloc(services.size(), sizeof(ServiceMetadata));  // TODO: Make this use mmap() + Shared Memory
+
+    volatile size_t i = 0;
+    for (std::string service_path : services) {
+        pid_t read_pid = fork();
+        if (read_pid) {
+            int status;
+            waitpid(read_pid, &status, 0);
+        } else {
+            void* service = dlopen(service_path.c_str(), RTLD_NOW);
+            if (!service) exit(1);
+            ServiceMetadata* md = parse_so(service);
+            if (!md) exit(1);
+            metadata[i] = *md;
+            exit(0);
+        }
+        i++;
+    }
 }
